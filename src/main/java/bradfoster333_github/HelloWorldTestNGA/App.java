c6bcec11a9889346738018ae8d94afb891ed6c8d
bradfoster333 <bradfoster333@gmail.com>
2018-02-19 21:31:38 -0800

    Add files via upload

---
+++ src/main/java/bradfoster333_github/HelloWorldTestNGA/App.java
@@ -0,0 +1,13 @@
+package bradfoster333_github.HelloWorldTestNGA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
